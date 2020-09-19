7a59c7c11835a45667fb48719ae81b6565ee85a2
Estéfane <53922973+tefscs@users.noreply.github.com>
2020-09-19 09:46:26 -0300

    Novocomit

--- PBL_SO.cpp
+++ PBL_SO.cpp
@@ -13,6 +13,8 @@ void *escritores(void * arg);
 void realizarEscrita(int numThread,int idArquivo);
 void modificarArquivo(int numThread,int idArquivo);
 void *leitores(void * arg);
+int checarEscritor(int idArquivo);
+void leitura(int idArquivo,int numThread);
 //----------STRUCT DE ESTADOS DOS ESCRITORES------------------------------------------------------------
 typedef struct estados{
 	int estado[QUANTIDADE];                                //Vetor para o estado da thread.
@@ -48,32 +50,19 @@ void *escritores(void * arg){
 	int *numThread= (int *) arg;
 	int indice, idArquivo;
 	while(1){
-		idArquivo=0;//rand()%3;  //Sorteia o número do arquivo a ler lido
+		idArquivo=rand()%3;  //Sorteia o número do arquivo a ler lido
 		pthread_mutex_lock(&(regiaoCritica)); //Entrando na regiaoCritica   ///Nem sei se assim ta certo, to meio bugada
 		estadosEscritores.estado[(*numThread)]= QUER_ESCREVER;
 		estadosEscritores.arquivo[(*numThread)]=idArquivo;
 		printf("\n-Escritor %d: Desejo escrever no arquivo %s",(*numThread),nomeArquivo[idArquivo]);
 		realizarEscrita((*numThread),idArquivo);
 		pthread_mutex_unlock(&(regiaoCritica));//saindo da regiao Critica
-		
-		//O QUE FAZEMOS QUEM TÁ ESPERANDO?????????????????????????????????????????
-		for(indice=0; indice<QUANTIDADE; indice++){//quem estava esperando para escrever no mesmo arquivo agora pode escrever
-	  		if(estadosEscritores.estado[indice]==QUER_ESCREVER && estadosEscritores.arquivo[indice]==idArquivo){
-	  			realizarEscrita(indice,idArquivo);
-		  	}
-		}
 	}
 }
 //--------------------------PENSAR EM UM NOME Q FAZ SENTIDO
 void realizarEscrita(int numThread,int idArquivo){
-  int indice=0;
-  int escrevendo=0; //Indica se há outro escrito escrevendo no mesmo arquivo desejado. escrevendo=1 tem outro escritor. escrevendo=0 não tem outro escritor ;
-  for(indice=0; indice<QUANTIDADE; indice++){
-  	if(estadosEscritores.estado[indice]==ESCREVENDO && estadosEscritores.arquivo[indice]==idArquivo){
-  		escrevendo=1;
-  		printf("\nNo exato momento o escritor %d esta escrevendo neste arquivo %s",indice, nomeArquivo[idArquivo]);//***depois pode apagar se quiser, só para verificacao no momento
-	  }
-  }
+  int escrevendo; //Indica se há outro escrito escrevendo no mesmo arquivo desejado. escrevendo=1 tem outro escritor. escrevendo=0 não tem outro escritor ;
+  escrevendo=checarEscritor(idArquivo);
   if(escrevendo==0){
   	estadosEscritores.estado[numThread]==ESCREVENDO; //Altera o estado do escritor para escrevendo, o que indica que nenhum escritor poderá modificar
   	modificarArquivo(numThread,idArquivo); //Modifica o arquivo, através da escrita
@@ -95,26 +84,50 @@ void modificarArquivo(int numThread, int idArquivo){
 	estadosEscritores.estado[numThread]==JA_ESCREVI; //Altera o estado do escritor
 }
 
-//----------FUNCAO LEITORES
+//----------FUNCAO LEITORES---------------------------------------------------------------
 /*Esta função realiza a leitura dos arquivos*/
 void *leitores(void * arg){
 	int *numThread= (int *) arg;
-	int idArquivo;
-	char texto[50];
-	FILE *ponteiroLeitura;
+	int idArquivo, haEscritor;
 	printf("\nO Leitor %d foi criado",*(numThread));
 	while(1){
-		idArquivo=0; //rand()% 3;	//Sorteia um numero aleatorio que determinará o nome do arquivo
-		ponteiroLeitura=fopen(nomeArquivo[idArquivo],"r");//Abre o arquivo em modo leitura
-		printf("\n-Leitor %d: Estou lendo o arquivo %d: %s",(*numThread),idArquivo,nomeArquivo[idArquivo]);
-		printf("\n---------------Conteudo do arquivo %s lido pelo Leitor %d------------------",nomeArquivo[idArquivo],(*numThread));
-		do{//Será lido enquanto não for o final do arquivo
-			fgets(texto,50,ponteiroLeitura); //Ler o texto
-			printf("\n\t%s",texto); //Imprime na tela
-		}while(feof(ponteiroLeitura)==0);
-		fclose(ponteiroLeitura); //fecha o arquivo
-		printf("\n-Leitor %d: Finalizei a leitura do arquivo %d: %s",(*numThread),idArquivo,nomeArquivo[idArquivo]);	
+		idArquivo=rand()% 3;	//Sorteia um numero aleatorio que determinará o nome do arquivo
+		printf("\n-Leitor %d: deseja ler o arquivo %d: %s",(*numThread),idArquivo,nomeArquivo[idArquivo]);
+		haEscritor=checarEscritor(idArquivo);
+		if(haEscritor==0){  //Se não tiver escritor escrevendo no mesmo aqruivo, o leitor pode ler
+			leitura(idArquivo,(*numThread));
+		}
+		else{ //Se houver escritor escrevendo o leitor não ler e imprime na tela a mensagem
+			printf("\n-Leitor %d: O arquivo desejado esta sendo escrito: %d: %s",(*numThread),idArquivo,nomeArquivo[idArquivo]);
+		}
 	}
+}
+//---------------FUNCAO QUE CHEGA SE TEM ESCRITOR NO MESMO ARQUIVO DESEJADO-------------------------------------------
+int checarEscritor(int idArquivo){ //Retorna 0 se não tem escritor no mesmo arquivo e 1 se tem escritor no arquivo.
+  int indice=0;
+  int escrevendo=0; //Indica se há outro escrito escrevendo no mesmo arquivo desejado. escrevendo=1 tem outro escritor. escrevendo=0 não tem outro escritor ;
+  while(indice<QUANTIDADE && escrevendo==0){
+  	if(estadosEscritores.estado[indice]==ESCREVENDO && estadosEscritores.arquivo[indice]==idArquivo){ //Se houver escrevendo no arquivo desejado
+  		escrevendo=1;                                                                                 //Muda o valor da variavel escrevendo para 1
+  		printf("\nNo exato momento o escritor %d esta escrevendo neste arquivo %s",indice, nomeArquivo[idArquivo]);//***depois pode apagar se quiser, só para verificacao no momento
+	  }
+	  indice++;  //incrementa a variável indice
+  }
+  return escrevendo;   
+}
+//------------------------------------LEITURA
+void leitura(int idArquivo,int numThread){
+	char texto[50];
+	FILE *ponteiroLeitura;
+	ponteiroLeitura=fopen(nomeArquivo[idArquivo],"r");//Abre o arquivo em modo leitura
+	printf("\n-Leitor %d: Estou lendo o arquivo %d: %s",numThread,idArquivo,nomeArquivo[idArquivo]);
+	printf("\n---------------Conteudo do arquivo %s lido pelo Leitor %d------------------",nomeArquivo[idArquivo],numThread);
+	do{//Será lido enquanto não for o final do arquivo
+		fgets(texto,50,ponteiroLeitura); //Ler o texto
+		printf("\n\t%s",texto); //Imprime na tela
+	}while(feof(ponteiroLeitura)==0);
+	fclose(ponteiroLeitura); //fecha o arquivo
+	printf("\n-Leitor %d: Finalizei a leitura do arquivo %d: %s",numThread,idArquivo,nomeArquivo[idArquivo]);
 }
 
 
